ebfe3a6d4f0f6ecea723349209184abe1d86080f
Freek Van der Herten <freek@spatie.be>
2025-08-23 13:51:25 +0200

    wip

--- config/url-ai-transformer.php
+++ config/url-ai-transformer.php
@@ -1,6 +1,31 @@
 <?php
 
 return [
+    /*
+     * The model that will be used to store the transformation results.
+     *
+     * You can use your own model by extending the default model.
+     */
+    'model' => Spatie\LaravelUrlAiTransformer\Models\TransformationResult::class,
+
+    /*
+     * The actions that will perform low-level operations of the package.
+     *
+     * You can extend the default actions and specify your own actions here
+     * to customize the package's behavior.
+     */
+    'actions' => [
+        'process_registration' => Spatie\LaravelUrlAiTransformer\Actions\ProcessRegistrationAction::class,
+    ],
+
+    /*
+     * By default, the transformers that ship with this package leverage the wonderful
+     * Prism package to interact with various AI services.
+     *
+     * https://prismphp.com
+     *
+     * You can customize the default settings here.
+     */
     'ai' => [
         'default' => [
             'provider' => Prism\Prism\Enums\Provider::OpenAI,
@@ -11,11 +36,5 @@ return [
             'provider' => Prism\Prism\Enums\Provider::OpenAI,
             'model' => 'dall-e-3',
         ],
-    ],
-
-    'model' => Spatie\LaravelUrlAiTransformer\Models\TransformationResult::class,
-
-    'actions' => [
-        'process_registration' => Spatie\LaravelUrlAiTransformer\Actions\ProcessRegistrationAction::class,
     ],
 ];
